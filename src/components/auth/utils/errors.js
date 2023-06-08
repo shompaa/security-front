07f280aeccde7fba088069944ff1cc083e75acd8
Shompa <nico_nahuelpan@hotmail.com>
2023-06-08 17:39:21 -0400

    add errors validation for login form

---
+++ src/components/auth/utils/errors.js
@@ -0,0 +1,6 @@
+export const errorValidation = {
+  400: "Error de datos",
+  401: "Las credenciales son incorrectas",
+  404: "El correo no se encuentra registrado",
+  500: "Error del servidor, contacte al administrador",
+};
